9d37903941ec81e582c12a8ea59620ec43835b94
PauloRev <paulohenriquerev@gmail.com>
2022-10-12 18:26:27 -0300

    Responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,32 @@
+import { useContract, useListings } from "@thirdweb-dev/react";
+import { Marketplace } from "@thirdweb-dev/sdk";
 import Head from "next/head";
 import { ChangeEvent, useCallback, useMemo, useState } from "react";
 import { NFTCard } from "./Components/NFTCard";
 
 export default function Home() {
-
   const [search, setSearch] = useState('');
+
+  const marketplace = useContract<Marketplace>('0x2e7bE08928915cBaB5E56bfF343469A6c8A11f25');
+
+  const { data: listings } = useListings(marketplace.contract);
 
   const handleSearch = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   }, []);
 
-  const filteredNfts = useMemo(() => {
-    return nfts.filter(nft => search ? nft.name.toLowerCase().includes(search.toLocaleLowerCase()) : true)
-  }, [search, nfts]);
+  const filteredListings = useMemo(() => {
+    return listings?.filter(nft => search ? nft.asset.name.toString().toLowerCase().includes(search.toLocaleLowerCase()) : true)
+  }, [search, listings]);
 
   return (
-    <div className="h-full w-screen bg-[#1d1f2b] px-24">
+    <div className="h-full w-screen bg-[#1d1f2b] px-[10%] min-h-[80vh]">
       <Head>
         <title>Coders NFT</title>
       </Head>
 
-      <div className="flex justify-between items-baseline">
-        <h2 className="text-5xl font-bold mt-24">
+      <div className="flex flex-wrap justify-between items-baseline">
+        <h2 className="text-4xl sm:text-5xl font-bold mt-24">
           Discover
         </h2>
 
@@ -31,14 +36,14 @@ export default function Home() {
       <hr className="w-full border-[#242634] mt-12" />
 
       <div className="flex-col items-start gap-7 mt-12">
-        <h2 className="text-5xl font-bold">
+        <h2 className=" text-4xl sm:text-5xl font-bold">
           Popular Bid
         </h2>
 
         <div className="flex flex-wrap items-start gap-7 mt-7 min-h-[50%]">
           {
-            filteredNfts.map(nft => (
-              <NFTCard nft={nft} key={nft.id} />
+            filteredListings?.map(listing => (
+              <NFTCard listing={listing} key={listing.id} />
             ))
           }
         </div>
@@ -47,39 +52,3 @@ export default function Home() {
     </div>
   )
 }
-
-// NFTS
-export const nfts = [
-  {
-    id: '1',
-    name: '#4473',
-    price: '82.55',
-    author: '0xE51B77159',
-    image: 'https://img.seadn.io/files/cfafbb97673c61ae4080ec8ce490bb83.png?auto=format&fit=max&w=384',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-  },
-  {
-    id: '2',
-    name: '#4474',
-    price: '83',
-    author: '0xE51B77159',
-    image: 'https://img.seadn.io/files/23b87cc5839bd426fb7516716b814b19.png?auto=format&fit=max&w=384',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-  },
-  {
-    id: '3',
-    name: '#4475',
-    price: '88.5',
-    author: '0xE51B77159',
-    image: 'https://img.seadn.io/files/4119dcd07c558db6179c653226d9855e.png?auto=format&fit=max&w=384',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-  },
-  {
-    id: '4',
-    name: '#44756',
-    price: '88.95',
-    author: '0xE51B77159',
-    image: 'https://img.seadn.io/files/b530abdd44b8b64ac686d2f02a6384b6.png?auto=format&fit=max&w=384',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-  },
-]
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,57 +1,98 @@
+import { useAddress, useContract, useListing, useListings, useNFT } from "@thirdweb-dev/react";
+import { Marketplace } from "@thirdweb-dev/sdk";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
 import { NFTCard } from "../Components/NFTCard";
-
-import { nfts } from '../index';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export default function NFTDetails() {
 
+  const [loadingPurchase, setLoadingPurchase] = useState(false);
+
   const router = useRouter();
 
-  const nft = nfts.find(nft => nft.id === router.query.id);
+  const marketplace = useContract<Marketplace>('0x2e7bE08928915cBaB5E56bfF343469A6c8A11f25');
+
+  const { data: listings, status } = useListings(marketplace.contract);
+  const listing = useListing(marketplace.contract, Number(router.query.id));
+
+  const { contract } = useContract(listing.data?.assetContractAddress);
+  const { data: nft } = useNFT(contract, listing.data?.asset.id);
+
+  const walletAddress = useAddress();
+  const userIsNFTOwner = walletAddress && walletAddress === nft?.owner;
+
+  const shouldDisabledByNFTButton = userIsNFTOwner || loadingPurchase || listing.data?.quantity.toString() === '0';
+
+  const handleByNFT = useCallback(async () => {
+    try {
+      setLoadingPurchase(true);
+
+      // const for get receipt with more transaction informations
+      await marketplace.contract.direct.buyoutListing(listing.data?.id, 1);
+
+      setLoadingPurchase(false);
+      Notify.success('You have successfully bought this NFT!');
+
+    } catch (err) {
+      // console.log('BUY ERROR => ', err);
+      setLoadingPurchase(false);
+      Notify.failure('Failed to buy this NFT!');
+    }
+  }, [listing]);
 
   return (
-    <div className="px-24">
+    <div className="px-[10%] min-h-[80vh]">
       <Head>
-        <title>{nft?.name} NFT - Details Page</title>
+        <title>{listing.data?.asset.name} - Details Page</title>
       </Head>
 
-      <div className="mt-24 flex justify-between">
-        <img src={nft?.image} alt={nft?.name} className="rounded-[1.25rem] w-[45%] h-auto" />
+      {
+        status === 'loading' ?
+          <h2 className="text-4xl text-center font-semibold mt-24">Loading...</h2>
+          :
+          <>
+            <div className="mt-24 flex justify-between flex-wrap gap-10 sm:gap-0">
+              <img src={listing.data?.asset.image} alt={listing.data?.asset.name.toString()} className="rounded-[1.25rem]  w-full max-w-md h-auto" />
 
-        <div className="ml-10 w-[45%]">
-          <h1 className="text-5xl font-bold">{nft?.name}</h1>
-          <p className="text-[#93989a] mt-4">{nft?.description}</p>
+              <div className="ml-10 w-full max-w-xl">
+                <h1 className="text-5xl font-bold">{listing.data?.asset.name}</h1>
+                <p className="text-[#93989a] mt-4">{listing.data?.asset.description}</p>
 
-          <hr className="w-full border-[#242634] mt-8 mb-4" />
+                <hr className="w-full border-[#242634] mt-8 mb-4" />
 
-          <div>
-            <p className="text-[#93989a]">Creator</p>
-            <p>{nft?.author}</p>
-          </div>
+                <div>
+                  <p className="text-[#93989a]">Owner</p>
+                  <p>{nft?.owner.slice(0, 6)} {userIsNFTOwner && '(You)'}</p>
+                </div>
 
-          <hr className="w-full border-[#242634] mt-8 mb-4" />
+                <hr className="w-full border-[#242634] mt-4 mb-8" />
 
-          <div>
-            <button className='right-1 bottom-0.5 bg-[#ff2748] py-[1rem] px-6 rounded-xl hover:scale-105 active:scale-95'>
-              Place a Bid
-            </button>
-          </div>
+                <div>
+                  <button disabled={shouldDisabledByNFTButton} className={`right-1 bottom-0.5 bg-[#ff2748] py-[1rem] px-6 rounded-xl ${shouldDisabledByNFTButton ? 'opacity-50' : 'hover:scale-105 active:scale-95'}`} onClick={handleByNFT}>
+                    {
+                      loadingPurchase ? 'Loading...' : 'Buy NFT'
+                    }
+                  </button>
+                </div>
 
-        </div>
-      </div>
+              </div>
+            </div>
 
-      <div className="mt-24">
-        <h2 className="text-4xl mt-24">More Works</h2>
+            <div className="mt-24">
+              <h2 className="text-4xl mt-24">More Works</h2>
 
-        <div className="flex flex-wrap items-start gap-16 mt-7">
-          {
-            nfts.filter((_nft, index) => index < 2).map(nft => (
-              <NFTCard nft={nft} key={nft.id} />
-            ))
-          }
-        </div>
-      </div>
+              <div className="flex flex-wrap items-start gap-16 mt-7">
+                {
+                  listings?.filter((_listing, index) => index < 2).map(listing => (
+                    <NFTCard listing={listing} key={listing.id} />
+                  ))
+                }
+              </div>
+            </div>
+          </>
+      }
 
     </div>
   )
